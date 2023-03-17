595983be4034e47509251d055ca0a454fae3e955
Tebogoivy <hubert.t.ivy@gmail.com>
2023-03-17 11:17:09 -0700

    Time for action

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the given line
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle\n");
+    return (0);
+}
