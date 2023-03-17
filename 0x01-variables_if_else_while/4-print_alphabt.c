64d10a97d55e2bceb1ebba697578e4843e49b576
Tebogoivy <hubert.t.ivy@gmail.com>
2023-03-17 15:11:53 -0700

    Time for action

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * main - Prints the alphabet, each letter on a new line.
+ * main - Prints the alphabet without q and e.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i < 52; i++)
+	for (i = 97; i < 123; i++)
 	{
-		putchar(alp[i]);
-		putchar('\n');
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
 	}
+	putchar('\n');
 	return (0);
 }
