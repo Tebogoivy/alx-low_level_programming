9ba5d596bfffd895dbaa8ea318260d226bfffed4
Tebogoivy <hubert.t.ivy@gmail.com>
2023-03-27 12:57:46 -0700

    Time for Pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,13 @@
 /**
-
-_strlen - Returns the length of a string
-@s: Pointer to the string
-Return: Length of the string
-*/
+ * _strlen - Returns the length of a string
+ * @s: Pointer to the string
+ *
+ * Return: Length of the string
+ */
 int _strlen(char *s)
 {
 int len = 0;
+
 while (*(s + len) != '\0')
 {
 len++;
