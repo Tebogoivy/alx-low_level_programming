fe2c0831676ade963d86a0f981520a11c2b8e7f0
Tebogoivy <hubert.t.ivy@gmail.com>
2023-03-27 12:38:49 -0700

    Time for action

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: Pointer to an integer
+ * @b: Pointer to an integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+}
