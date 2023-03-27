b73905563675207558f9ffbf84c87b3b20bb45ae
Tebogoivy <hubert.t.ivy@gmail.com>
2023-03-27 12:31:00 -0700

    Time for action

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,10 @@
--Wall -Werror -Wextra -pedantic -std=gnu89
+/**
+ * reset_to_98 - updates the value of the integer it points to to 98.
+ *
+ * @n: a pointer to the integer to be updated.
+ *
+ * Return: void.
+ */
 void reset_to_98(int *n)
 {
 *n = 98;
