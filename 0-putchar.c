716a8dfdf56932fcff351ac13d88d8c57536d524
Tebogoivy <hubert.t.ivy@gmail.com>
2023-03-28 13:58:45 -0700

    Time for Pointers

---
+++ 0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints _putchar, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    putchar('_');   /* prints the underscore character */
+    putchar('p');   /* prints the "p" character */
+    putchar('u');   /* prints the "u" character */
+    putchar('t');   /* prints the "t" character */
+    putchar('c');   /* prints the "c" character */
+    putchar('h');   /* prints the "h" character */
+    putchar('a');   /* prints the "a" character */
+    putchar('r');   /* prints the "r" character */
+    putchar('\n');  /* prints a new line character */
+    return (0);     /* returns 0 to indicate successful execution */
+}
