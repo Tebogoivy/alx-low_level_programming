5f944ecf6ba326bbd2db4f8659a1e0ba3a690d16
Tebogoivy <hubert.t.ivy@gmail.com>
2023-03-28 11:36:49 -0700

    Time for Pointers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,12 @@
-#include "main.h"
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
  */
-void _puts(char *str)
+int main(void)
 {
-while (*str != '\0')
-{
-_putchar(*str++);
-}
-_putchar('\n');
+char *str;
+
+str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+_puts(str);
+return (0);
 }
