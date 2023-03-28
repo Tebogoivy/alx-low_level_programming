68aaef01533c6bf39811695fb540a4e1a15d5b71
Tebogoivy <hubert.t.ivy@gmail.com>
2023-03-28 11:39:19 -0700

    Time for Pointers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,9 +4,7 @@
  */
 int main(void)
 {
-char *str;
-
-str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-_puts(str);
-return (0);
+    char *str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
 }
