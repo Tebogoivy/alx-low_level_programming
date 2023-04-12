3f47adb120a5b2fe2bedcc44948d568a8f704b3d
Tebogoivy <hubert.t.ivy@gmail.com>
2023-04-12 12:54:12 -0700

    Malloc

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,79 +1,49 @@
 #include "main.h"
-		
 #include <stdlib.h>
-		
+
 /**
-		
- * argstostr - main entry
-		
- * @ac: int input
-		
- * @av: double pointer array
-		
- * Return: 0
-		
+ * argstostr - concatenate all arguments into a single string
+ *
+ * @ac: number of arguments
+ * @av: array of arguments
+ *
+ * Return: pointer to concatenated string
  */
-		
 char *argstostr(int ac, char **av)
-		
 {
-		
-	int i, n, r = 0, l = 0;
-		
-	char *str;
-		
+    int i, j, len = 0, k = 0;
+    char *str;
 
-		
-	if (ac == 0 || av == NULL)
-		
-		return (NULL);
-		
+    if (ac == 0 || av == NULL)
+        return (NULL);
 
-		
-	for (i = 0; i < ac; i++)
-		
-	{
-		
-		for (n = 0; av[i][n]; n++)
-		
-			l++;
-		
-	}
-		
-	l += ac;
-		
+    /* Calculate total length of concatenated string */
+    for (i = 0; i < ac; i++)
+    {
+        for (j = 0; av[i][j]; j++)
+            len++;
+        len++;
+    }
 
-		
-	str = malloc(sizeof(char) * l + 1);
-		
-	if (str == NULL)
-		
-		return (NULL);
-		
-	for (i = 0; i < ac; i++)
-		
-	{
-		
-	for (n = 0; av[i][n]; n++)
-		
-	{
-		
-		str[r] = av[i][n];
-		
-		r++;
-		
-	}
-		
-	if (str[r] == '\0')
-		
-	{
-		
-		str[r++] = '\n';
-		
-	}
-		
-	}
-		
-	return (str);
-		
+    /* Allocate memory for concatenated string */
+    str = malloc(sizeof(char) * (len + 1));
+    if (str == NULL)
+        return (NULL);
+
+    /* Copy each argument into concatenated string */
+    for (i = 0; i < ac; i++)
+    {
+        for (j = 0; av[i][j]; j++)
+        {
+            str[k] = av[i][j];
+            k++;
+        }
+        str[k] = '\n';
+        k++;
+    }
+
+    /* Add null terminator */
+    str[k] = '\0';
+
+    return (str);
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,85 +1,52 @@
 #include "main.h"
-		
 #include <stdlib.h>
-		
+
 /**
-		
- * alloc_grid - nested loop to make grid
-		
- * @width: width input
-		
- * @height: height input
-		
- * Return: pointer to 2 dim. array
-		
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ *
+ * Return: concat of s1 and s2
  */
-		
-int **alloc_grid(int width, int height)
-		
+char *str_concat(char *s1, char *s2)
 {
-		
-	int **mee;
-		
-	int x, y;
-		
+    char *conct;
+    int i, ci;
 
-		
-	if (width <= 0 || height <= 0)
-		
-		return (NULL);
-		
+    if (s1 == NULL)
+        s1 = "";
 
-		
-	mee = malloc(sizeof(int *) * height);
-		
+    if (s2 == NULL)
+        s2 = "";
 
-		
-	if (mee == NULL)
-		
-		return (NULL);
-		
+    i = ci = 0;
 
-		
-	for (x = 0; x < height; x++)
-		
-	{
-		
-		mee[x] = malloc(sizeof(int) * width);
-		
+    while (s1[i] != '\0')
+        i++;
 
-		
-		if (mee[x] == NULL)
-		
-		{
-		
-			for (; x >= 0; x--)
-		
-				free(mee[x]);
-		
+    while (s2[ci] != '\0')
+        ci++;
 
-		
-			free(mee);
-		
-			return (NULL);
-		
-		}
-		
-	}
-		
+    conct = malloc(sizeof(char) * (i + ci + 1));
 
-		
-	for (x = 0; x < height; x++)
-		
-	{
-		
-		for (y = 0; y < width; y++)
-		
-			mee[x][y] = 0;
-		
-	}
-		
+    if (conct == NULL)
+        return (NULL);
 
-		
-	return (mee);
-		
+    i = ci = 0;
+
+    while (s1[i] != '\0')
+    {
+        conct[i] = s1[i];
+        i++;
+    }
+
+    while (s2[ci] != '\0')
+    {
+        conct[i] = s2[ci];
+        i++, ci++;
+    }
+
+    conct[i] = '\0';
+
+    return (conct);
 }
