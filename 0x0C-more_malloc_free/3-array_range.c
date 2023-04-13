e9353192befe8bd4e4a4fa272015294c33efa2e0
Tebogoivy <hubert.t.ivy@gmail.com>
2023-04-13 11:49:45 -0700

    Malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,59 +1,48 @@
 #include <stdlib.h>
-		
 #include "main.h"
-		
 
-		
+
 /**
-		
- * *array_range - creates an array of integers
-		
- * @min: minimum range of values stored
-		
- * @max: maximum range of values stored and number of elements
-		
+ * _memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
  *
-		
- * Return: pointer to the new array
-		
+ * Return: pointer to the memory area s
  */
-		
-int *array_range(int min, int max)
-		
+char *_memset(char *s, char b, unsigned int n)
 {
-		
-	int *ptr;
-		
-	int i, size;
-		
+    unsigned int i;
 
-		
-	if (min > max)
-		
-		return (NULL);
-		
+    for (i = 0; i < n; i++)
+    {
+        s[i] = b;
+    }
 
-		
-	size = max - min + 1;
-		
+    return (s);
+}
 
-		
-	ptr = malloc(sizeof(int) * size);
-		
 
-		
-	if (ptr == NULL)
-		
-		return (NULL);
-		
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    char *ptr;
 
-		
-	for (i = 0; min <= max; i++)
-		
-		ptr[i] = min++;
-		
+    if (nmemb == 0 || size == 0)
+        return (NULL);
 
-		
-	return (ptr);
-		
+    ptr = malloc(size * nmemb);
+
+    if (ptr == NULL)
+        return (NULL);
+
+    _memset(ptr, 0, nmemb * size);
+
+    return (ptr);
 }
